cefd8cef638dfa844c8089be0cf2fcf3e597def9
User <lyamzin01@mail.ru>
2023-10-29 15:07:15 +0300

    dorabotka

--- src/Main.java
+++ src/Main.java
@@ -12,49 +12,71 @@ public class Main {
         System.out.println(box[2]);
 
 
-        double [ ] corn = {1.57, 7.654, 9,986};
+        double [ ] corn = {1.57, 7.654, 9.986};
         System.out.println(corn [0]);
         System.out.println(corn [1]);
         System.out.println(corn [2]);
 
 
-        int [] hotel = new int [6];
+        int [] hotel = new int [3];
         hotel [0] = 50_000_00;
         hotel [1] = 30_000_00;
-        hotel [5]= 20_000_000;
-        System.out.println(hotel[5]);
+        hotel [2]= 20_000_000;
+        System.out.println(hotel[2]);
 
 
         // Задача 2
 
-        for (int i = 0; i < 3; i++) {
-            System.out.print(box[i] + ", ");
+        for (int i = 0; i < box.length; i++) {
+            System.out.print(box[i]);
+            if (i != box.length - 1 ) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
-        for (int i = 0; i < 3; i++) {
-            System.out.print(corn[i] + ", ");
+        for (int i = 0; i < corn.length; i++) {
+            System.out.print(corn[i]);
+            if (i != corn.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
-        for (int i = 0; i <= 5; i++) {
+        System.out.println();
 
-            System.out.print(hotel[i] + ", ");
+        for (int i = 0; i < hotel.length; i++) {
+            System.out.print(hotel[i]);
+            if (i != hotel.length - 1) {
+                System.out.print(", ");
+
+            }
         }
+
+        System.out.println();
         System.out.println();
 
         //Задача 3
-        for (int i = 2; i >= 0 ; i--) {
-            System.out.print(box[i] + ", ");
+        for (int i = box.length - 1; i >= 0 ; i--) {
+            System.out.print(box[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
-        for (int i = 3; i >=0; i--) {
-            System.out.print(corn[i] + ", ");
+        for (int i = corn.length- 1; i >=0; i--) {
+            System.out.print(corn[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
-        for (int i = 5; i >=0; i--) {
+        for (int i = hotel.length - 1; i >=0; i--) {
+            System.out.print(hotel[i]);
+            if (i == 0){
+                break;
+            }
+            System.out.print(", ");
+        }
 
-            System.out.print(hotel[i] + ", ");
-        }
-        System.out.println();
 
         //Задача 4
 int [] arr = {1, 2, 3};
